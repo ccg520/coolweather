66f5abb9d5db9556ff0f05ce444364c645074824
CaoCaiGui <ccg510@163.com>
2018-01-31 21:07:06 +0800

    加入数据库创建和表的各项配置

---
+++ app/src/main/java/com/study/coolweather/CoolWeatherApplication.java
@@ -0,0 +1,19 @@
+package com.study.coolweather;
+
+import android.app.Application;
+
+import org.litepal.LitePal;
+
+/**
+ * Created by root on 18-1-31.
+ */
+
+public class CoolWeatherApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LitePal.initialize(this);
+    }
+}
